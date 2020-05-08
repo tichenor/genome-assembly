a1161af64adc52d725664428ecb5def877853c98
tichenor <xesufen@gmail.com>
2020-05-08 11:52:05 +0200

    Made an abstract line parsing task that can be extended with specific functionality. Tested this with a FindExclusionTask that checks whether one contig is contained in another, in which case the index of the file(chunk) and the line position is stored in a hash map. Tested it on the first 10 chunks (100k lines/overlaps per chunk; one million lines/overlaps total) with 2 threads: Found about 50-60 thousand exclusions per chunk (more than half!). Test took about 500 ms. Seems viable to run on the whole data set, should take less than a minute.

--- src/LineParserParallel.java
+++ src/LineParserParallel.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -23,27 +22,23 @@ public class LineParserParallel {
     private ExecutorService threadPool;
     private AtomicInteger counter = new AtomicInteger(0);
     public Set<String> identifiers;
-    public Hashtable<String, Integer> exclusions;
+    public Map<Integer, Set<Integer>> exclusions;
 
+    /**
+     * Base class for a runnable task to parse each line of a specific file. To be run by another thread.
+     * The format for the filename needs to be adjusted if the directory or any filenames are modified.
+     */
+    abstract class LineParseTask implements Runnable {
 
-    class LineParseTask implements Runnable {
+        protected int fileIndex;
+        protected int linePosition;
 
-        private String targetDir;
-        private int fileIndex;
-
-        public LineParseTask(String targetDir, int fileIndex) {
-            this.targetDir = targetDir;
+        public LineParseTask(int fileIndex) {
             this.fileIndex = fileIndex;
+            linePosition = 0;
         }
 
-        private void doSomethingWithLine(String line) {
-
-            String[] fields = line.split(delimiter);
-            // identifiers.add(fields[0]);
-            // identifiers.add(fields[1]);
-
-
-        }
+        protected abstract void doSomethingWithLine(String line);
 
         @Override
         public void run() {
@@ -53,6 +48,7 @@ public class LineParserParallel {
                 FileReader fr = new FileReader(file);
                 BufferedReader br = new BufferedReader(fr);
                 while ((line = br.readLine()) != null) {
+                    linePosition++;
                     doSomethingWithLine(line);
                 }
                 br.close();
@@ -63,22 +59,58 @@ public class LineParserParallel {
         }
     }
 
+    /**
+     * Runnable task that finds exclusions for contig overlaps. If one contig is contained in the other, the task
+     * stores the line position (data point) in a HashMap. The key is the file index and the value is a set of line
+     * positions (integers).
+     */
+    class FindExclusionTask extends LineParseTask {
+
+        public FindExclusionTask(int fileIndex) {
+            super(fileIndex);
+        }
+
+        protected void doSomethingWithLine(String line) {
+            String[] fields = line.split(delimiter);
+            // Check if one contig is contained in the other by checking if the overlap is the whole contig.
+            if (fields[5].equals("0") && fields[6].equals(fields[7])
+                    || fields[9].equals("0") && fields[10].equals(fields[11])) {
+                // First contig contained in second or vice versa, note the position of the exclusion.
+                exclusions.computeIfAbsent(fileIndex, k -> new HashSet<>()); // Make new set if there is none yet.
+                exclusions.get(fileIndex).add(linePosition); // Add the line position to the set of exclusions for the file.
+            }
+        }
+    }
+
+    /**
+     * Parse multiple text files using one or more parallel threads, possibly storing results.
+     *
+     * @param targetDir Target directory relative to the project root.
+     * @param filenamePrefix If the files have a common beginning, e.g. "file01, file02, ...".
+     * @param numFiles The number of files to process.
+     * @param numThreads The number of threads to use.
+     */
     public LineParserParallel(String targetDir, String filenamePrefix, int numFiles, int numThreads) {
         this.targetDir = targetDir;
         this.filenamePrefix = filenamePrefix;
         this.numFiles = numFiles;
         this.numThreads = numThreads;
-        identifiers = Collections.synchronizedSet(new HashSet<>());
+        // identifiers = Collections.synchronizedSet(new HashSet<>());
     }
 
-    public void parseFiles() throws InterruptedException {
-        ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
+    /**
+     * The method that does the parsing.
+     */
+    public void findExclusions() {
+        exclusions = Collections.synchronizedMap(new HashMap<>()); // Initialize storage for exclusions.
+        ExecutorService threadPool = Executors.newFixedThreadPool(numThreads); // Create thread pool
         for (int i = 0; i < numFiles; i++) {
-            threadPool.submit(new LineParseTask(targetDir, i)); // File index starts at 0
+            // Submit a runnable task for each file.
+            threadPool.submit(new FindExclusionTask(i)); // File index starts at 0
         }
-        threadPool.shutdown();
+        threadPool.shutdown(); // Make the thread pool not accept any further tasks.
         try {
-            threadPool.awaitTermination(60L, TimeUnit.SECONDS);
+            threadPool.awaitTermination(60L, TimeUnit.SECONDS); // Wait for tasks to finish.
         } catch (InterruptedException e) {
             // Happens if the thread pool is interrupted while waiting for threads to finish.
             e.printStackTrace();
@@ -86,18 +118,15 @@ public class LineParserParallel {
     }
 
     public static void main(String[] args) {
-        LineParserParallel lpp = new LineParserParallel("res/splits/", "chunk", 641, 8);
+        LineParserParallel lpp = new LineParserParallel("res/splits/", "chunk", 10, 2);
         long start = System.nanoTime();
-        try {
-            lpp.parseFiles();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        lpp.findExclusions();
         long end = System.nanoTime();
         long dur = TimeUnit.NANOSECONDS.toMillis((end - start));
         System.out.println("Parse took " + dur + " ms.");
-        System.out.println("Test found " + lpp.identifiers.size() + " identifiers.");
-
+        for (Map.Entry<Integer, Set<Integer>> entry : lpp.exclusions.entrySet()) {
+            System.out.println("Found " + entry.getValue().size() + " exclusions in file " + entry.getKey());
+        }
     }
 
 }
