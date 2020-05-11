56234dab8f0a586b0dd609733f2586939beb007c
tichenor <xesufen@gmail.com>
2020-05-11 10:29:07 +0200

    Main class runs a full indexing of all filtered chunks, then generates the full graph. Then the degree distribution and connected components are computed and the results are written to two text files.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,4 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
 
 public class Main {
 
@@ -12,13 +6,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Map<String, Integer> indices = CustomReader.indexAllIdentifiers();
-        System.out.println("Size: "+ indices.size());
+        // Index all unique string identifiers to integers.
+        System.out.println("Indexing identifiers...");
+        long start = System.nanoTime();
+        Map<String, Integer> indices = LineParser.indexAllIdentifiers();
+        long end = System.nanoTime();
+        System.out.println();
+        CustomWriter.testDuration(start, end);
+        System.out.println("Unique identifiers found: "+ indices.size());
+        System.out.println("--------------------");
 
-        Graph g = CustomReader.generateFullGraph(indices);
+        // Generate a graph from the integer indices.
+        System.out.println("Generating graph from indices...");
+        start = System.nanoTime();
+        Graph graph = LineParser.generateFullGraph(indices);
+        end = System.nanoTime();
+        System.out.println();
         System.out.println("Graph generated.");
-        System.out.println("Vertices: " + g.numberOfVertices());
-        System.out.println("Edges: " + g.numberOfEdges());
+        CustomWriter.testDuration(start, end);
+        System.out.println("Vertices: " + graph.numberOfVertices());
+        System.out.println("Edges: " + graph.numberOfEdges());
+        System.out.println("--------------------");
+
+        // The following block computes and writes the degree frequencies to a text file.
+        System.out.println("Finding degree distribution...");
+        start = System.nanoTime();
+        Map<Integer, Integer> degFreqs = graph.getDegreeDistribution();
+        end = System.nanoTime();
+        CustomWriter.testDuration(start, end);
+        System.out.println("Found " + degFreqs.size() + " different degree values of graph.");
+        CustomWriter.writeMapToFile(degFreqs, degFreqs.size() + 1, "degreeFrequencies",
+                "Degree frequencies -- (degree):(vertex count) -- " + degFreqs.size() + " entries.");
+        System.out.println("--------------------");
+
+        // The following block computes writes the components and their sizes to a text file.
+        System.out.println("Finding connected components...");
+        start = System.nanoTime();
+        List<Integer> components = graph.findConnectedComponents();
+        end = System.nanoTime();
+        CustomWriter.testDuration(start, end);
+        System.out.println("Found " + components.size() + " connected components.");
+        CustomWriter.writeListToFile(components, components.size() + 1, "components",
+                "Connected components -- (component number):(number of vertices) -- " + components.size() +
+                        " components.");
+        System.out.println("--------------------");
+        System.out.println("Program finished. Garbage collection might take a few seconds.");
 
         // Counting the lines (data points) of the whole file.
 //        final String filename = "res/Spruce_fingerprint_2017-03-10_16.48.olp.m4";
