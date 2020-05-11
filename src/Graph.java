1ae4b6eaef995aa762861a5cd3cc169670dbbfce
tichenor <xesufen@gmail.com>
2020-05-11 10:31:09 +0200

    Implemented a method to find connected components that uses an iterative depth first search.

--- src/Graph.java
+++ src/Graph.java
@@ -11,8 +11,13 @@ public class Graph implements Serializable {
 
    List<List<Integer>> graph;
 
+    /**
+     * The number of vertices is passed into the constructor in order to allocate the necessary memory
+     * for the array capacity. Once an instance is created, the number of vertices cannot be changed.
+     * @param numVertices The number of vertices.
+     */
    public Graph(int numVertices) {
-       graph = new ArrayList<>(numVertices);
+       graph = new ArrayList<>(numVertices); // Set initial capacity of the array so it doesn't need to dynamically resize.
        for (int i = 0; i < numVertices; i++) {
            graph.add(new ArrayList<>());
        }
@@ -32,15 +37,7 @@ public class Graph implements Serializable {
        for (List<Integer> adjList : graph) {
            sum += adjList.size();
        }
-       return sum / 2;
-   }
-
-   public int[] getAllDegrees() {
-       int[] degrees = new int[graph.size()];
-       for (int i = 0; i < graph.size(); i++) {
-           degrees[i] = graph.get(i).size();
-       }
-       return degrees;
+       return sum / 2; // Each edge is counted twice in an undirected graph.
    }
 
     /**
@@ -62,4 +59,40 @@ public class Graph implements Serializable {
        return degFrequencies;
    }
 
+    /**
+     * Compute the number of connected component as well as their size (number of vertices) using
+     * an iterative depth first search.
+     * @return A list of integers corresponding to component sizes. The size of the array is the number of components.
+     */
+   public List<Integer> findConnectedComponents() {
+       List<Integer> components = new ArrayList<>();
+       boolean[] visited = new boolean[graph.size()]; // Initialize all vertices as unvisited
+       int index = 0; // Index of current component being processed
+       for (int i = 0; i < graph.size(); i++) { // Go through every vertex
+           if (!visited[i]) {
+               components.add(0); // Add a new component of size 0
+               depthFirstSearch(i, visited, components, index); // Do a depth first search on each unvisited vertex
+               index++;
+           }
+       }
+       return components;
+   }
+
+   private void depthFirstSearch(int vertex, boolean[] visited, List<Integer> components, int currentComponent) {
+       Stack<Integer> stack = new Stack<>();
+       stack.push(vertex);
+       while (!stack.empty()) {
+           int v = stack.pop();
+           if (!visited[v]) {
+               int newValue = components.get(currentComponent) + 1;
+               components.set(currentComponent, newValue); // Increment the component size by one as we visit v.
+               visited[v] = true;
+           }
+           for (int adjacent : graph.get(v)) {
+               if (!visited[adjacent]) {
+                   stack.push(adjacent); // Push any neighbors onto the stack.
+               }
+           }
+       }
+   }
 }
