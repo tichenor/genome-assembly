2d6fe2d99b413d7d70284bf98edb6232049c3803
tichenor <xesufen@gmail.com>
2020-05-11 10:34:33 +0200

    Wrote a function for writing the contents of a list to a text file for a simple way to store results. Also wrote a method for tracking progress of longer function executions.

--- src/CustomWriter.java
+++ src/CustomWriter.java
@@ -1,12 +1,14 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class CustomWriter {
 
-    public static <A, B> void writeMapToFile(Map<A, B> map, int entriesToWrite, String filename) {
+    public static <K, V> void writeMapToFile(Map<K, V> map, int entriesToWrite, String filename, String header) {
 
         FileWriter fileWriter;
         BufferedWriter out;
@@ -15,9 +17,8 @@ public class CustomWriter {
             fileWriter = new FileWriter("res/results/" + filename + ".txt");
             out = new BufferedWriter(fileWriter);
             int count = 0;
-            Iterator<Map.Entry<A, B>> iterator = map.entrySet().iterator();
-            while (iterator.hasNext() && count < entriesToWrite) {
-                Map.Entry<A, B> entry = iterator.next();
+            out.write(header + "\n");
+            for (Map.Entry<K, V> entry : map.entrySet()) {
                 out.write(entry.getKey().toString() + ":" + entry.getValue().toString() + "\n");
                 count++;
             }
@@ -25,5 +26,46 @@ public class CustomWriter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static <T> void writeListToFile(List<T> list, int entriesToWrite, String filename, String header) {
+
+        FileWriter fileWriter;
+        BufferedWriter out;
+
+        try {
+            fileWriter = new FileWriter("res/results/" + filename + ".txt");
+            out = new BufferedWriter(fileWriter);
+            int count = 0;
+            out.write(header + "\n");
+            ListIterator<T> iter = list.listIterator();
+            while (iter.hasNext()) {
+                int index = iter.nextIndex();
+                T elem = iter.next();
+                out.write(index + ":" + elem.toString() + "\n");
+                count++;
+            }
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateProgress(double progressPercentage) {
+        final int width = 50;
+        System.out.print("\r[");
+        int i = 0;
+        for (; i < (int) (progressPercentage * width); i++) {
+            System.out.print(".");
+        }
+        for (; i < width; i++) {
+            System.out.print(" ");
+        }
+        System.out.print("]");
+    }
+
+    public static void testDuration(long start, long end) {
+        long duration = TimeUnit.NANOSECONDS.toMillis(end - start);
+        System.out.println("Time elapsed: " + duration + " milliseconds.");
     }
 }
